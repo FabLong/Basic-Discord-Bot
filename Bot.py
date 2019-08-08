00f758d2dff2610b686a096f7b23b8db7d19a9df
Fab <fabienjlong@gmail.com>
2019-08-08 21:17:29 +0100

    V1
    Not so helpful discord bot guide for python!

---
+++ Bot.py
@@ -0,0 +1,71 @@
+# Made by: Fab Long ©
+# https://github.com/FabLong/Basic-Discord-Bot << more info here
+
+#imports
+import discord
+import time
+
+
+#Keep track of your bot version here
+version = "1.0.0"
+
+# Read from server configuration file [server.txt]
+# Required#####################################################
+def read_token():
+    with open("servers/server.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+def read_server_id():
+    with open("servers/server.txt", "r") as f:
+        lines = f.readlines()
+        return lines[1].strip()
+# Not required# But useful######################################
+def read_channels():
+    with open("servers/server.txt", "r") as f:
+        lines = f.readlines()
+        return lines[2].strip()
+def read_users():
+    with open("servers/server.txt", "r") as f:
+        lines = f.readlines()
+        return lines[3].strip()
+################################################################
+
+# Store important server information
+token = read_token()
+server_id = int(read_server_id())
+channels = str(read_channels())
+valid_users = str(read_users())
+
+# ease of use
+client = discord.Client()
+
+########################################################################################################################
+
+# Checking Messages in Discord Chat
+@client.event
+async def on_message(message):
+
+    if message.content == "!help":
+        embed = discord.Embed(title="Bot Commands Help", description="Available commands")
+        embed.add_field(name="!help", value="shows help menu")
+        embed.add_field(name="?bothelp", value="Admin {Bot configuration GitHub}")
+        embed.add_field(name="!test", value="Checks Bot's status")
+        await message.channel.send(content=None, embed=embed)
+
+
+    # can be used to gather server info // [guild == server]
+    id = client.get_guild(server_id)
+
+
+    # Bot Commands
+    if message.content == "?bothelp":
+        await message.channel.send("visit: https://github.com/FabLong/Basic-Discord-Bot")
+    # get time and version printed in server
+    elif message.content == "!test":
+        # the message below is embedded into the Discord chat using ```[TEXT-HERE]```
+        await message.channel.send(f"```Cloud: Online\nTime: {time.ctime()}\nVersion: {version}```")
+
+########################################################################################################################
+
+# Starts Bot
+client.run(token)
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Basic-Discord-Bot
+
+STEP1: Donwload Bot.py and folder: servers
+STEP2: Read below STEPS to help install discord.py
+STEP3: Open server.txt file in servers folder. Change what's inside so it is specific to you. [It assumes yout bot is setup in discord developers section]
+STEP4: Run Bot and test it works.
+STEP5: Edit your bot to your liking :)
+
+Helpful links to get you started:
+	https://discordapp.com/developers << create bot // Bot token HERE
+	https://discordapi.com/permissions.html << create Bot link to join it to server
+	https://discordpy.readthedocs.io/en/latest/api.html << discord.py documentation
+	
+	
+Needs:
+	@python 3.6 NOT 3.7. You can use Anaconda to do this: https://www.anaconda.com/distribution/ OR just unistall python 3.7 and install 3.6
+	
+	@install discord.py << 1. GET pip: https://www.liquidweb.com/kb/install-pip-windows/ 2. using CMD: cd into your python directory 3. using CMD: DO: pip install discord
+	
+	
+Useful:
+	pycharm is easy to stop and start Bot (and allows you to use the Anaconda Distribution)
+	
+	
+	
+
+Stuck?! Try online: Google/Youtube/stackoverflow...
